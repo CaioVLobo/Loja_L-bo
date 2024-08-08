1885ce6986af75e7004b5dda1bd895383bbd930b
CaioVLobo <Caio.viannalobo6@gmail.com>
2024-08-08 15:47:44 -0300

    Primeira Versão da Loja

---
+++ Index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="src/Index.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index - Loja Lôbo</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="Index.html">Index</a>
+            <a href="Produtos.html">Produtos</a>
+            <a href="Contatos.html">Contatos</a>
+            <a href="SobreNos.html">Sobre Nós</a>
+        </nav>
+    </header>
+</body>
+</html>
---
+++ SobreNos.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <link rel="stylesheet" href="src/SobreNos.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre Nós - Loja Lôbo</title>
+</head>
+
+<body>
+    <header style="width: 840px;">
+        <h1>Sobre Nós</h1>
+    </header>
+
+    <div class="container">
+        <h2>Sobre Nós</h2>
+        <p>Bem-vindo à Doceria Doce amor, onde o carinho é o ingrediente principal em todas as nossas receitas! Fundada
+            em 2021.2, nossa doceria tem o prazer de oferecer uma ampla variedade de doces, bolos e sobremesas feitos
+            com ingredientes frescos e de alta qualidade.</p>
+
+        <p>Nossos chefs talentosos e apaixonados trabalham arduamente para criar deliciosas obras-primas que encantam os
+            paladares de nossos clientes. Além de saborosos bolos de aniversário e cupcakes decorados, também oferecemos
+            uma variedade de opções veganas, sem glúten e sem açúcar para atender às necessidades de todos os nossos
+            clientes.</p>
+
+        <p>No coração de nossa doceria está o compromisso com a satisfação do cliente. Estamos dedicados a proporcionar
+            uma experiência doce e memorável a todos os que entram em nossa loja. Seja para uma ocasião especial ou
+            apenas para um desejo repentino de doce, estamos aqui para tornar cada momento mais doce e especial.</p>
+
+        <p>Agradecemos por escolher a nossa doceria e esperamos ter o prazer de servi-lo em breve!</p>
+
+        <p><strong>Horário de Funcionamento:</strong><br>
+            Segunda a Sexta: 13h - 18:10h<br>
+            Sábado: 7:30h - 12:30h<br>
+            Domingo: Fechado<br>
+
+        </p>
+    </div>
+    <ul>
+        <li><a href="Principal.html">Home</a></li>
+    </ul>
+
+</body>
+
+</html>
